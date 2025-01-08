27c7c029dd714f6e040093ab300ddbb1dc2c7dc8
sahiltcg <sahilahmedtcg@gmail.com>
2025-01-08 12:34:09 +0530

    Reacts updated

---
+++ VS code/src/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function button() {
+  return (
+    <div className='bg-violet-600 px-6 py-2 text-slate-800 text-sm cursor-pointer rounded-xl hover:ease-in hover:bg-sky-600'>Button</div>
+  )
+}
+
+export default button
---
+++ VS code/src/Sqr.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Button from './button'
+function div() {
+  return (
+    <div className='flex items-center justify-center w-10 h-10 p-20 rounded-tl-3xl rounded-br-3xl bg-rose-700 
+    text-2xl text-white font-bold border-2 border-purple-400 hover:rounded-none hover:rounded-tr-3xl hover:rounded-bl-3xl ease-in duration-300 hover:bg-lime-900'>
+      <Button />
+    </div>
+  )
+}
+
+export default div
